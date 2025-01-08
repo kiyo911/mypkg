e85c5f064cce11798b4d2c6a4e72cd4858ed87cf
Hiromu Kiyokawa <hirosan315911@gmail.com>
2025-01-09 03:37:30 +0900

    github test

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,9 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg fibonacci_listen.launch.py > /tmp/mypkg.log
+timeout 20 ros2 launch mypkg fibonacci_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
+grep 'Listen: 1'
 grep 'Listen: 13'
+grep 'Listen: 1597'
