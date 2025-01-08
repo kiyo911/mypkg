1cc270277829cdbf31ebb05f7309e91626a5bd69
Hiromu Kiyokawa <hirosan315911@gmail.com>
2025-01-09 02:50:11 +0900

    launch <- initinal commit

---
+++ launch/fibonacci_listen.launch.py
@@ -0,0 +1,19 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+
+    fibonacci = launch_ros.actions.Node(
+        package='mypkg',
+        executable='fibonacci',
+        )
+    listener = launch_ros.actions.Node(
+        package='mypkg',
+        executable='Listener',
+        output='screen'
+        )
+
+    return launch.LaunchDescription([fibonacci, listener])
