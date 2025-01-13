8e45c4dc1607dfbe45cf3525a33bdb29445eab06
Hiromu Kiyokawa <hirosan315911@gmail.com>
2025-01-14 04:50:11 +0900

    test.bash <- テストの記述の修正

--- test/test.bash
+++ test/test.bash
@@ -8,16 +8,30 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 30 ros2 launch mypkg fibonacci_listen.launch.py > /tmp/mypkg.log
+timeout 3 ros2 launch mypkg fibonacci_listen.launch.py > /tmp/mypkg.log
+
+res=0
+
+ng () {
+        echo ${1}行目が違うよ
+        res=1
+}
 
 cat /tmp/mypkg.log |
-grep 'Listen: 1'
+grep ' 1'
+
+[ "$?" = 0 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 cat /tmp/mypkg.log |
-grep 'Listen: 13'
+grep ' 3'
 
-cat /tmp/mypkg.log |
-grep 'Listen: 1597'
+[ "$?" = 0 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-cat /tmp/mypkg.log |
-grep 'Listen: 1836311903'
+
+if [ "$res" -eq 0 ]; then
+  echo "OK"
+fi
+
+exit $res
